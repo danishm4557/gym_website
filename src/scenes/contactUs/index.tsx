52ccb13606a4834922f2d520c923d7d9268a2afa
danishm4557 <91935259+danishm4557@users.noreply.github.com>
2024-12-12 19:44:09 -0600

    Updated pattern for email address validation in form

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -93,7 +93,10 @@ const ContactUs = ({ setSelectedPage } : Props) => {
 						// SET INPUT ATTRIBUTES
 						{...register("email", {
 							required: true,
-							pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+							pattern: {
+								value: /\S+@\S+\.\S+/,
+								message: "Entered value does not match email format",
+							}
 						})}
 					/>
 					{/* ERROR VALIDATION AND TEXT IF ERROR IS THROWN */}
